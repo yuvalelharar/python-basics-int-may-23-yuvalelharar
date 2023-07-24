ff4f5ff33846bcbdf128d458aab87357ea591ddd
yuvalelharar <yuvalelharar1@gmail.com>
2023-07-24 18:24:21 +0300

    adding class 04

---
+++ 04_aritmetic_operation.py
@@ -0,0 +1,29 @@
+number_1 = 9
+number_2 = 4
+
+text_1 = "yuval"
+text_2 = "elharar"
+
+print(f"{text_1} + {text_2} = {(text_1 + text_2)}")
+
+print(f"{number_1} + {number_2} = {(number_1 + number_2)}")
+print(number_1, " + ", number_2, " = ", (number_1 + number_2))
+
+print(f"{number_1} - {number_2} = {(number_1 - number_2)}")
+
+text_1 = "**"
+print(f"{text_1} * {number_2} = {(text_1 * number_2)}")
+
+print(f"{number_1} * {number_2} = {(number_1 * number_2)}")
+print(type(number_1 * number_2))
+
+number_3 = 2.0
+
+print(f"{number_1} * {number_3} = {(number_1 * number_3)}")
+print(type(number_1 * number_3))
+
+print(f"{number_1} / {number_2} = {(number_1 / number_2)}")
+
+number_1 = 2
+number_2 = 5
+print(f"{number_1} ** {number_2} = {(number_1 ** number_2)}")
