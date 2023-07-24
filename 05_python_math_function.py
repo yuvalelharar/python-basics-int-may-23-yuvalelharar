bbfc2df289baa7f6c865d3c7bedcae3a59b07105
yuvalelharar <yuvalelharar1@gmail.com>
2023-07-24 18:54:52 +0300

    05 class

---
+++ 05_python_math_function.py
@@ -0,0 +1,30 @@
+# abs()
+
+number = abs(-56.4)
+print(number)
+print(abs(-123))
+print(abs(34))
+
+#min(x.y) & max(x,y)
+
+numbers = (23, 3, 45, -97, 0)
+print("the minimum number is: ", min(numbers))
+print("the maximum number is: ", max(numbers))
+
+#pow(x, y) --> x^y
+
+number1 = 2
+number2 = 4
+print("2 ^ 4 = ", pow(number1, number2))
+
+#round(x)
+
+number3 = 5.6715897
+print("the round of", number3, "is: ", round(number3))
+print("the round of", number3, "is: ", round(number3, 2))
+
+#square(x)
+
+number4 = 25
+print(pow(number4, 0.5))
+
