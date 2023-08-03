e853685a1c29ff6e057a85c0bd0ef4b532ae1fca
yuvalelharar <yuvalelharar1@gmail.com>
2023-08-03 18:16:34 +0300

    new lesson

---
+++ 04_assignment_operators.py
@@ -0,0 +1,21 @@
+# operator +
+number = 10
+print(f"number = {number}")
+
+#number = number+1
+
+number += 1
+print(f"number = number +1 \n ---> number = {number}")
+
+# operator -
+
+number1 = 3
+print(f"number = {number1}")
+number1 -= 1
+print(f"number -=1 \n----->number= {number1}")
+
+# operator *
+number1 = 3
+print(f"number = {number1}")
+number1 *= 9
+print(f"number *=9 \n----->number= {number1}")
