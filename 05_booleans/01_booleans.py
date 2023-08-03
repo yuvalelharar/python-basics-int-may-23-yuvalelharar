77411a7bcbadb235cec64bcf90a3b7b5aaefa98b
yuvalelharar <yuvalelharar1@gmail.com>
2023-08-03 19:26:14 +0300

    boolean type

---
+++ 05_booleans/01_booleans.py
@@ -0,0 +1,6 @@
+status = True
+my_status = False
+print(status)
+print(type(status))
+print(my_status)
+print(type(my_status))
--- class_task/practice1.py
+++ class_task/practice1.py
@@ -1,3 +1,13 @@
+#1
+
+
 number = int(input(f"Please enter a number : \n"))
 number += 1
 print(number)
+
+
+#2
+number = float(input(f"Please enter a number: \n"))
+number *= 1.5
+print(number)
+
