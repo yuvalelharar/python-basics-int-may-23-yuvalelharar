d80e2b984d9d5ff7ed48732a27106361aa46d651
yuvalelharar <yuvalelharar1@gmail.com>
2023-08-17 20:27:42 +0300

    class 17/08

---
+++ 06_03_string_methods/06_class_practice.py
@@ -0,0 +1,12 @@
+#Targil1
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print(f" HELLO {name.upper()}. you are {age} years old")
+
+#Targil2
+
+a, b =map(int, input("Enter 2 numbers: ").split())
+print(f"screen resolution: {a} x {b}.\nTotal resolution of pixel = {a * b}")
+
---
+++ 06_03_string_methods/methods_basics.py
@@ -0,0 +1,13 @@
+massage = "HELLO WORLD"
+massage2 = "yuval elharar;26"
+
+print(massage)
+print(type(massage))
+
+print(massage.lower()) #אותיות קטנות
+print(massage2.upper()) #אותיות גדולות
+print(massage2.capitalize()) #אות ראשונה גדולה
+print(massage2.title()) #אות ראשונה בכל מילה גדולה
+print(massage2.split()) #הפרדת ביטוי לרשימת מילים
+print(massage2.split(';')) #מחפש נקודה פסיק ומכניסה לאיבר
+
